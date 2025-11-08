b312f881b92118deca82271e09dd48fe6c1c7e26
Minh Le Vo Nhat <lvnminh@gmail.com>
2025-11-08 15:32:20 +0700

    delete redundant function in post controller and service

--- apps/classes-service/src/modules/posts/posts.controller.ts
+++ apps/classes-service/src/modules/posts/posts.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Inject, Injectable, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { PostsDto } from '../dto/post-response.dto';
 
 @Controller('posts')
 export class PostsController {
@@ -12,8 +10,4 @@ export class PostsController {
     return 'Hello from Posts Controller!';
   }
 
-  @MessagePattern('posts.add_new_post')
-  async addNewPost(@Payload() newPost: PostsDto) {
-    return this.postsService.addNewPost(newPost.class_id, newPost.message, newPost.sender_id, newPost.parent_id, newPost.title);
-  }
 }
