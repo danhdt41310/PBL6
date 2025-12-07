b153235971cf673197d3deb96e17feb4826c3c84
danhnv <danhnv@htplus.software>
2025-12-07 20:54:14 +0700

    refactor exam service

--- apps/exams-service/src/modules/questions/questions.module.ts
+++ apps/exams-service/src/modules/questions/questions.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
+import { QuestionsRepository } from './questions.repository';
 import { QuestionsImportService } from './questions-import.service';
 import { PrismaService } from '../../prisma/prisma.service';
 import { TransactionService } from '../../prisma/transaction.service';
 
 @Module({
   controllers: [QuestionsController],
-  providers: [QuestionsService, QuestionsImportService, PrismaService, TransactionService],
+  providers: [QuestionsService, QuestionsRepository, QuestionsImportService, PrismaService, TransactionService],
   exports: [QuestionsService],
 })
 export class QuestionsModule {}
--- apps/exams-service/src/modules/submission-answers/submission-answers.module.ts
+++ apps/exams-service/src/modules/submission-answers/submission-answers.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SubmissionAnswersController } from './submission-answers.controller';
 import { SubmissionAnswersService } from './submission-answers.service';
+import { SubmissionAnswersRepository } from './submission-answers.repository';
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   controllers: [SubmissionAnswersController],
-  providers: [SubmissionAnswersService, PrismaService],
+  providers: [SubmissionAnswersService, SubmissionAnswersRepository, PrismaService],
   exports: [SubmissionAnswersService],
 })
 export class SubmissionAnswersModule {}
